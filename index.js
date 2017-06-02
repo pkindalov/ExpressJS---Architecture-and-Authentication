149ef4cc8849915b87865d574b54bc4053bbc7ff
pkindalov <panayotkindalov@gmail.com>
2017-06-02 03:21:48 +0300

    Express configurated

--- index.js
+++ index.js
@@ -3,18 +3,10 @@ let env = process.env.NODE_ENV || 'development'
 let settings = require('./server/config/settings')[env]
 
 const express = require('express')
-const handlebars = require('express-handlebars')
-
-require('./server/config/database')(settings)
-
 let app = express()
 
-app.engine('handlebars', handlebars({
-  defaultLayout: 'main'
-}))
-app.set('view engine', 'handlebars')
-
-app.use(express.static('public'))
+require('./server/config/database')(settings)
+require('./server/config/express')(app)
 
 app.get('/', (req, res) => {
   res.render('index')
