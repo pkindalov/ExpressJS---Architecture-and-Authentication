dcf9c1f2c7b281c63b70d7cb64f5e30e91f3f23e
pkindalov <panayotkindalov@gmail.com>
2017-06-02 03:13:55 +0300

    Split information to the two new files - database and settings.js in server/config directory

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
+let env = process.env.NODE_ENV || 'development'
+
+let settings = require('./server/config/settings')[env]
+
 const express = require('express')
-const mongoose = require('mongoose')
 const handlebars = require('express-handlebars')
 
-mongoose.Promise = global.Promise
-let env = process.env.NODE_ENV || 'development'
-let port = process.env.PORT || 1337
+require('./server/config/database')(settings)
+
 let app = express()
 
 app.engine('handlebars', handlebars({
@@ -15,13 +17,8 @@ app.set('view engine', 'handlebars')
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  mongoose
-    .connect('mongodb://localhost:27017/generictemplate')
-    .then(() => {
-      console.log('MongoDB ready')
-      res.render('index')
-    })
+  res.render('index')
 })
 
-app.listen(port)
-console.log(`Server listen on port ${port}`)
+app.listen(settings.port)
+console.log(`Server listen on port ${settings.port}`)
