a7fbed315e71e276139c8216bb1e5c9071e8d196
pkindalov <panayotkindalov@gmail.com>
2017-06-03 14:28:22 +0300

    Updated file user-controller.  Filled method registerPost. Added const encryption and User

--- server/controllers/users-controller.js
+++ server/controllers/users-controller.js
@@ -1,3 +1,5 @@
+const encryption = require('../utilities/encryption')
+const User = require('mongoose').model('User')
 
 module.exports = {
   registerGet: (req, res) => {
@@ -5,6 +7,30 @@ module.exports = {
   },
 
   registerPost: (req, res) => {
+    let reqUser = req.body
 
+    // add validations
+    // (if reqUser.username.length < 3)...
+
+    let salt = encryption.generateSalt()
+    let hashedPassword = encryption.generateHashedPassword(salt, reqUser.password)
+
+    User.create({
+      username: reqUser.username,
+      firstName: reqUser.firstName,
+      lastName: reqUser.lastName,
+      salt: salt,
+      hashedPassword: hashedPassword
+
+    }).then(user => {
+      req.logIn(user, (err, user) => {
+        if (err) {
+          res.locals.globalError = err
+          res.render('/users/register', user)
+        }
+
+        res.redirect('/')
+      })
+    })
   }
 }
