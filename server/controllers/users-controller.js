747ab12ac9bfee71a6f3e3f8a29c548f30a6659b
pkindalov <panayotkindalov@gmail.com>
2017-06-03 17:47:18 +0300

    Added login form in folder users. Edited and fixed some errors in user-controller.

--- server/controllers/users-controller.js
+++ server/controllers/users-controller.js
@@ -20,17 +20,51 @@ module.exports = {
       firstName: reqUser.firstName,
       lastName: reqUser.lastName,
       salt: salt,
-      hashedPassword: hashedPassword
-
+      hashedPass: hashedPassword
     }).then(user => {
       req.logIn(user, (err, user) => {
         if (err) {
           res.locals.globalError = err
-          res.render('/users/register', user)
+          res.render('users/register', user)
         }
 
         res.redirect('/')
       })
     })
+  },
+  loginGet: (req, res) => {
+    res.render('users/login')
+  },
+
+  loginPost: (req, res) => {
+    let reqUser = req.body
+    User
+      .findOne({ username: reqUser.username }).then(user => {
+        if (!user) {
+          res.locals.globalError = 'Invalid user data'
+          res.render('users/login')
+          return
+        }
+
+        if (!user.authenticate(reqUser.password)) {
+          res.locals.globalError = 'Invalid user data'
+          res.render('users/login')
+          return
+        }
+
+        req.logIn(user, (err, user) => {
+          if (err) {
+            res.locals.globalError = err
+            res.render('users/login')
+          }
+
+          res.redirect('/')
+        })
+      })
+  },
+
+  logout: (req, res) => {
+    req.logout()
+    res.redirect('/')
   }
 }
