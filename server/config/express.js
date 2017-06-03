357d8d64e2691cb237053f94aa908492b0ca5a17
pkindalov <panayotkindalov@gmail.com>
2017-06-03 13:50:01 +0300

    Updated express.js files. Added cookieParser , bodyParser, session, passport, handlebars

--- server/config/express.js
+++ server/config/express.js
@@ -1,12 +1,25 @@
-const handlebars = require('express-handlebars')
+
 const express = require('express')
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
+const session = require('express-session')
+const passport = require('passport')
+const handlebars = require('express-handlebars')
 
 module.exports = (app) => {
   app.engine('handlebars', handlebars({
     defaultLayout: 'main'
   }))
   app.set('view engine', 'handlebars')
-
+  app.use(cookieParser())
+  app.use(bodyParser.urlencoded({ extended: true }))
+  app.use(session({
+    secret: 'neshto-taino!@#$%',
+    resave: false,
+    saveUninitialized: false
+  }))
+  app.use(passport.initialize())
+  app.use(passport.session())
   app.use(express.static('public'))
 
   console.log('Express ready!')
